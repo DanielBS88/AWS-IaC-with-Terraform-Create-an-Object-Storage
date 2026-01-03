a208a057fb4b0efff7a910d58064ce50be14901a
admin <daniel_brito@epam.com>
2026-01-02 22:15:55 -0300

    fix: add default values to variables for automated validation
    - Add default values to aws_region, project_tag, and bucket_name
    - Ensures terraform plan runs without interactive prompts
    - terraform.tfvars becomes optional override

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "project_tag" {
   description = "Project tag value to be applied to all resources"
   type        = string
+  default     = "cmtr-k5vl9gpq"
 }
 
 variable "bucket_name" {
   description = "Name of the S3 bucket to be created"
   type        = string
+  default     = "cmtr-k5vl9gpq-bucket-1767401280"
 }
